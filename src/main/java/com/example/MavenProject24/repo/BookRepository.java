12900330cb046e0d95a0f36843a35cc76f5b2cf8
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2025-09-02 11:15:23 +0530

    Update BookRepository.java

--- src/main/java/com/example/MavenProject24/repo/BookRepository.java
+++ src/main/java/com/example/MavenProject24/repo/BookRepository.java
@@ -8,8 +8,7 @@ import com.example.MavenProject24.entity.Book;
 public interface BookRepository extends CrudRepository<Book, Integer> {
 
     // Custom query methods for search and filter
-    List<Book> findByNameContainingIgnoreCaseOrAuthorContainingIgnoreCase(String name, String author);
-    List<Book> findByStatus(String status);
+  
     List<Book> findByNameContainingIgnoreCaseAndStatusOrAuthorContainingIgnoreCaseAndStatus(String name, String nameStatus, String author, String authorStatus);
 
 }
