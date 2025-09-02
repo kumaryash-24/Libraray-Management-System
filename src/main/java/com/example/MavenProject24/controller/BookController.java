3c2eb9fccae5774e06f7a84fbab42a2dbbd5432d
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2025-09-02 11:14:01 +0530

    Update BookController.java

--- src/main/java/com/example/MavenProject24/controller/BookController.java
+++ src/main/java/com/example/MavenProject24/controller/BookController.java
@@ -4,7 +4,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;                 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
